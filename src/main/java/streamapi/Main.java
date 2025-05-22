a673be37efa0dae11b6577ba403d9280dbe30971
ben-274 <ben-274@gmx.de>
2025-05-22 18:49:36 +0200

    Methode resources Funktionalitaet als Stream implementiert

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -88,31 +89,15 @@ public class Main {
      * @return String of all matching lines, separated by {@code "\n"}
      */
     public static String resources(String path) {
-        // TODO
-        StringBuilder result = new StringBuilder();
-
         try (InputStream stream = getResourceAsStream(path)) {
-            BufferedReader r = new BufferedReader(new InputStreamReader(stream));
-
-            List<String> allLines = new ArrayList<>();
-
-            String newLine = r.readLine();
-            while (newLine != null) {
-                allLines.add(newLine);
-                newLine = r.readLine();
-            }
-
-            for (int i = 1; i < allLines.size(); i++) {
-                String s = allLines.get(i);
-                if (s.startsWith("a") && !(s.length() < 2)) {
-                    result.append(allLines.get(i)).append("\n");
-                }
-            }
+            return new BufferedReader(new InputStreamReader(stream))
+            		.lines()
+            		.filter(line -> line.startsWith("a") && !(line.length() < 2))
+            		.collect(Collectors.joining("\n"));
 
         } catch (IOException e) {
             System.err.println("Ouch, that didn't work: \n" + e.getMessage());
+            return "";
         }
-
-        return result.toString();
     }
 }

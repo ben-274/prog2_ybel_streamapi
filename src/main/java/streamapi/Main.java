8d8e6704ffd9164faf9c6f9270f58b4a17cb7fc4
ben-274 <ben-274@gmx.de>
2025-05-22 16:00:27 +0200

    Methode random Funktionalitaet als Stream implementiert

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -2,6 +2,8 @@ package streamapi;
 
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -59,20 +61,7 @@ public class Main {
     public static List<Integer> random() {
         Random r = new Random();
 
-        // TODO
-        List<Integer> randomIntegers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            randomIntegers.add(r.nextInt(10));
-        }
-
-        List<Integer> returnList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            if (randomIntegers.get(i) % 2 == 0) {
-                returnList.add(randomIntegers.get(i) * randomIntegers.get(i));
-            }
-        }
-
-        return returnList;
+        return Stream.generate(() -> r.nextInt(10)).limit(10).filter(n -> n % 2 == 0).map(n -> n * n).collect(Collectors.toList());
     }
 
     /**

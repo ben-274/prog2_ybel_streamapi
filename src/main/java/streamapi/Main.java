dc0bca243170c861dcf16343e1d3ef58ef347056
ben-274 <ben-274@gmx.de>
2025-05-22 13:25:38 +0200

    Methode ifmCps funktionalitaet als stream umgesetzt

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -2,6 +2,7 @@ package streamapi;
 
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -53,16 +54,7 @@ public class Main {
      */
     public static Set<Integer> ifmCps(List<Student> studentList) {
         // TODO
-        Set<Integer> result = new HashSet<>();
-        Integer i = 0;
-        for (Student v : studentList) {
-            if (v.isIFM()) {
-                i = v.cps();
-                result.add(i);
-            }
-        }
-
-        return result;
+    	return studentList.stream().filter(Student::isIFM).map(Student::cps).collect(Collectors.toSet());
     }
 
     /**
